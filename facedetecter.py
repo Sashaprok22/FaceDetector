8b2eefd120ad3b7e81796cd91890a71cfbf990c4
Sashaprok <sashaprok22@gmail.com>
2022-09-14 09:40:38 +0700

    Убрана не нужная строка

--- facedetecter.py
+++ facedetecter.py
@@ -1,5 +1,4 @@
 import cv2
-from cv2 import CascadeClassifier
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eyes_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
